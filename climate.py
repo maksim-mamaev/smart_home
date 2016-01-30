9aacb5ccf5f1082015b820cbc82073a598858161
max <maksim.mamaev@gmail.com>
2016-01-30 13:01:19 +0300

    -day/night temperatures -normally open/close modes -refactoring

--- climate.py
+++ climate.py
@@ -5,18 +5,23 @@ import time
 from datetime import datetime, timedelta, date, time as dt_time
  
 FREQ = 300 # 5 min
-f = True
 run = True
+heatStatus = -1 # -1-onStart;0-off;1-on
+
+relayPin = 17
+w1_address = "28-00000477f239"
+normallyOpen = True
 
 #22-22.5 - it's cold for the night +0.5, may be ok for the day
-#22.5-23 - is ok, try to decrease -0.25
-minTemp = 22.5
-maxTemp = 23
-heatStatus = -1 # -1-onStart;0-off;1-on
+#22.5-23 - is ok fot the night
+dayMinTemp = 22
+dayMaxTemp = 22.5
+nightMinTemp = 22.5
+nightMaxTemp = 23
 
 def get_temp():
 	# Open the file that we viewed earlier so that python can see what is in it. Re$
-	tfile = open("/sys/devices/w1_bus_master1/28-00000477f239/w1_slave")
+	tfile = open("/sys/devices/w1_bus_master1/" + w1_address + "/w1_slave")
 	# Read all of the text in the file.
 	text = tfile.read()
 	# Close the file now that the text has been read.
@@ -32,21 +37,58 @@ def get_temp():
 
 	return temperature
 
-def heat(b):
-	try: 
+def heat(heat_on):
+	pin_status = GPIO.LOW
+
+	if heat_on:
+		log('Heat on')
+
+		if normallyOpen:
+			pin_status = GPIO.LOW
+		else:
+			pin_status = GPIO.HIGH
+	else:
+		log('Heat off')
+
+		if normallyOpen:
+			pin_status = GPIO.HIGH
+		else:
+			pin_status = GPIO.LOW
+
+	try:
 		# Use the pin numbers from the ribbon cable board.
 		GPIO.setmode(GPIO.BCM)
-		# Set up the pin you are using ("18" is an example) as output.
-		GPIO.setup(17, GPIO.OUT)
-		# Turn on the pin and see the LED light up.
-		if b == 1:
-			GPIO.output(17, GPIO.LOW)
-		else:
-			# Turn off the pin to turn off the LED.
-			GPIO.output(17, GPIO.HIGH)
-	finally:		
+
+		# Set up the pin as output.
+		GPIO.setup(relayPin, GPIO.OUT)
+
+		GPIO.output(relayPin, pin_status)
+
+	finally:
 		pass
 #		GPIO.cleanup()
+
+def get_min_temperature():
+	d = datetime.today()
+	h = d.hour
+
+	if h >= 0 and h <= 7:
+		resT = nightMinTemp
+	else:
+		resT = dayMinTemp
+
+	return resT
+
+def get_max_temperature():
+	d = datetime.today()
+	h = d.hour
+
+	if h >= 0 and h <= 7:
+		resT = nightMaxTemp
+	else:
+		resT = dayMaxTemp
+
+	return resT
 
 def nobody_home():
 	d = datetime.today()
@@ -66,21 +108,22 @@ while(run):
         try:
                 time.sleep(FREQ)
 		
-		t = get_temp();
-		
-		log(time.strftime("%Y-%m-%d %H:%M ") + 't=' + str(t) + ';nobody_home='+str(nobody_home())+';heat_status(-1-onStart;0-off;1-on)='+str(heatStatus))
-		
-		if(t < minTemp and not nobody_home()):
+		curT = get_temp()
+		minT = get_min_temperature()
+		maxT = get_max_temperature()
+
+		params = (time.strftime("%Y-%m-%d %H:%M"), str(curT), str(minT), str(maxT), str(nobody_home()), str(heatStatus))
+		log('{0} curT={1};minT={2};minT={3};nobody_home={4};heat_status(-1-onStart;0-off;1-on)={5}'.format(*params))
+
+		if(curT < minT and not nobody_home()):
 			if(heatStatus != 1):
-				heat(1)
+				heat(True)
 				heatStatus = 1
-				log('Heat on')
-				
-		elif(t > maxTemp or nobody_home()):
+
+		elif(curT > maxT or nobody_home()):
 			if(heatStatus != 0):
-				heat(0)
+				heat(False)
 				heatStatus = 0
-				log('Heat off')
 
         except KeyboardInterrupt:
                 run = False
